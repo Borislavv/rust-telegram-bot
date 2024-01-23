a56d5c167d6c3bc6729da2b8612da6276a9a8392
Boris <Glazunov2142@icloud.com>
2024-01-23 14:02:07 +0300

    chore: improved gateway/contract.

--- src/domain/telegram/gateway/contract/gateway.rs
+++ src/domain/telegram/gateway/contract/gateway.rs
@@ -1,13 +1,13 @@
 use crate::domain::telegram::gateway::contract::request;
-use crate::domain::telegram::gateway::contract::response;
+use crate::domain::telegram::gateway::{contract, dto};
 
 use serde_traitobject::Box;
 
 pub trait GatewayInterface {
-    // receving messages from telegram channel
-    fn get_messages(&self, request: Box<dyn request::GetMessagesDtoInterface>) 
-        -> Result<Box<dyn response::GetMessagesDtoInterface>, String>;
+    // receiving messages from telegram channel
+    fn get_messages(&self, request: Box<dyn contract::request::GetMessagesDtoInterface>)
+        -> Result<dto::response::GetMessagesDto, String>;
     // sending message to telegram channel
     fn send_message(&self, request: Box<dyn request::SendMessageDtoInterface>) 
-        -> Result<Box<dyn response::SendMessageDtoInterface>, String>;
+        -> Result<dto::response::SendMessageDto, String>;
 }
