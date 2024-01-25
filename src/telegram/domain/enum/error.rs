60bc49b1ad6d991f2ab07d41455c14e269c329f4
Boris <Glazunov2142@icloud.com>
2024-01-25 15:45:00 +0300

    chore: domain errors.

--- src/telegram/domain/enum/error.rs
+++ src/telegram/domain/enum/error.rs
@@ -1,5 +1,6 @@
 #[derive(Debug)]
-enum Error {
-    HttpRequestError(reqwest::Error),
-    TelegramError(String)
+pub enum Error {
+    InternalError(Box<dyn std::error::Error>),
+    TelegramError(Box<dyn std::error::Error>),
+    RepositoryError(Box<dyn std::error::Error>),
 }
