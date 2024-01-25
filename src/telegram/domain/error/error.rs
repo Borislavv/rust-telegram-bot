23a13cfa292a426ceda57a633d827a28612e1624
Boris <Glazunov2142@icloud.com>
2024-01-25 15:45:00 +0300

    chore: domain errors.

--- src/telegram/domain/error/error.rs
+++ src/telegram/domain/error/error.rs
@@ -1,19 +1,22 @@
-use crate::domain::telegram::gateway::r#enum::Error;
+use crate::telegram::domain::r#enum;
+use std::fmt;
 
-impl fmt::Display for Error {
+impl fmt::Display for r#enum::error::Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {
-            Error::HttpRequestError(ref err) => write!(f, "HTTP request error: {}", err),
-            Error::TelegramError(ref err) => write!(f, "Telegram gateway error: {}", err),
+            r#enum::error::Error::InternalError(ref err) => write!(f, "Internal error occurred: {}", err),
+            r#enum::error::Error::TelegramError(ref err) => write!(f, "Telegram API error occurred: {}", err),
+            r#enum::error::Error::RepositoryError(ref err) => write!(f, "Http repository error occurred: {}", err),
         }
     }
 }
 
-impl std::error::Error for Error {
+impl std::error::Error for r#enum::error::Error {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match *self {
-            Error::HttpRequestError(ref err) => Some(err),
-            Error::TelegramError(_) => None,
+            r#enum::error::Error::InternalError(ref err) => Some(err.as_ref()),
+            r#enum::error::Error::TelegramError(ref err) => Some(err.as_ref()),
+            r#enum::error::Error::RepositoryError(ref err) => Some(err.as_ref()),
         }
     }
 }
