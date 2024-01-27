b661953e727c25489fa874b5d6818dae03004aa8
Boris <Glazunov2142@icloud.com>
2024-01-27 12:18:02 +0300

    chore: improved receiver.

--- src/telegram/app/service/receiver/receiver.rs
+++ src/telegram/app/service/receiver/receiver.rs
@@ -1,19 +1,21 @@
 use std::rc::Rc;
+use crate::telegram::app::service::receiver::receiver_interface::MessageReceiver;
 use crate::telegram::domain::{contract, model, repository};
+use crate::telegram::domain::r#enum::error::Error;
 
 pub struct Receiver {
     repo: Rc<dyn repository::interface::Telegram>
 }
 
 impl Receiver {
-    pub fn new(repo: Rc<dyn repository::interface::Telegram>) -> Self
-    {
-        return Self { repo }
+    pub fn new(repo: Rc<dyn repository::interface::Telegram>) -> Self {
+        Self { repo }
     }
+}
 
-    pub fn get_messages(&self, request: Box<dyn contract::request::GetMessagesDtoInterface>)
-        -> Result<model::response::GetMessagesDto, String>
-    {
-        return self.repo.get_messages(request);
+impl MessageReceiver for Receiver {
+    fn get_messages(&self, request: Box<dyn contract::request::GetMessagesDtoInterface>)
+        -> Result<model::response::GetMessagesDto, Error> {
+        self.repo.get_messages(request)
     }
 }
