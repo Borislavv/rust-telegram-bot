845042bbdbe3084a19408d5198f893def6328a58
Boris <Glazunov2142@icloud.com>
2024-01-27 12:18:21 +0300

    chore: improved sender.

---
+++ src/telegram/app/service/sender/sender.rs
@@ -0,0 +1,23 @@
+use std::fmt::Debug;
+use std::rc::Rc;
+use serde::Serialize;
+use crate::telegram::app::service::sender::sender_interface::MessageSender;
+use crate::telegram::domain::{contract, model, repository};
+use crate::telegram::domain::r#enum::error::Error;
+
+pub struct Sender {
+    repo: Rc<dyn repository::interface::Telegram>
+}
+
+impl Sender {
+    pub fn new(repo: Rc<dyn repository::interface::Telegram>) -> Self {
+        Self { repo }
+    }
+}
+
+impl MessageSender for Sender {
+    fn send_message(&self, request: Box<dyn contract::request::SendMessageDtoInterface>)
+        -> Result<model::response::SuccessSendMessageDto, Error> {
+        self.repo.send_message(request)
+    }
+}
