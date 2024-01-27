8f01450a3923daffd7310bc4f4ed7c04dcd74bbc
Boris <Glazunov2142@icloud.com>
2024-01-27 12:20:25 +0300

    chore: removed dead code.

--- src/telegram/app/facade_interface.rs
+++ src/telegram/app/facade_interface.rs
@@ -1,6 +1,6 @@
-use std::fmt::Debug;
-use serde::Serialize;
-use crate::telegram::domain::{contract, model};
+
+
+
 use super::service::sender::sender_interface::MessageSender;
 use super::service::receiver::receiver_interface::MessageReceiver;
 pub trait TelegramFacadeInterface: MessageSender + MessageReceiver {
