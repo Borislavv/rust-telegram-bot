f37c5f2567d3eb97f3123b52b8871ba2a27f63e3
Boris <Glazunov2142@icloud.com>
2024-01-27 12:18:54 +0300

    chore: expanded errors.

--- src/telegram/domain/error/error.rs
+++ src/telegram/domain/error/error.rs
@@ -1,5 +1,7 @@
 use crate::telegram::domain::r#enum;
 use std::fmt;
+use std::str::Utf8Error;
+use reqwest::Error;
 
 impl fmt::Display for r#enum::error::Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -7,16 +9,32 @@ impl fmt::Display for r#enum::error::Error {
             r#enum::error::Error::InternalError(ref err) => write!(f, "Internal error occurred: {}", err),
             r#enum::error::Error::TelegramError(ref err) => write!(f, "Telegram API error occurred: {}", err),
             r#enum::error::Error::RepositoryError(ref err) => write!(f, "Http repository error occurred: {}", err),
+            r#enum::error::Error::EncodeDecodeUTF8Error(ref err) => write!(f, "Encode/Decode UTF8 error occurred: {}", err),
+            r#enum::error::Error::SerializeDeserializeError(ref err) => write!(f, "Serialize/Deserialize error occurred: {}", err),
         }
     }
 }
 
-impl std::error::Error for r#enum::error::Error {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        match *self {
-            r#enum::error::Error::InternalError(ref err) => Some(err.as_ref()),
-            r#enum::error::Error::TelegramError(ref err) => Some(err.as_ref()),
-            r#enum::error::Error::RepositoryError(ref err) => Some(err.as_ref()),
-        }
+impl From<String> for r#enum::error::Error {
+    fn from(value: String) -> Self {
+        r#enum::error::Error::InternalError(value)
+    }
+}
+
+impl From<reqwest::Error> for r#enum::error::Error {
+    fn from(value: Error) -> Self {
+        r#enum::error::Error::RepositoryError(value.to_string())
+    }
+}
+
+impl From<serde_json::Error> for r#enum::error::Error {
+    fn from(value: serde_json::Error) -> Self {
+        r#enum::error::Error::SerializeDeserializeError(value.to_string())
+    }
+}
+
+impl From<Utf8Error> for r#enum::error::Error {
+    fn from(value: Utf8Error) -> Self {
+        r#enum::error::Error::EncodeDecodeUTF8Error(value.to_string())
     }
 }
