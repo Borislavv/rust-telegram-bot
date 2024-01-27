284afdd12f3669cbc09b4df52c5d31a61651604e
Boris <Glazunov2142@icloud.com>
2024-01-27 10:21:03 +0300

    chore: telegram http repository.

--- src/telegram/infrastructure/repository/http.rs
+++ src/telegram/infrastructure/repository/http.rs
@@ -1,17 +1,14 @@
 use std::collections::HashMap;
-use std::error::Error;
-use std::fmt::Debug;
 
 use reqwest::blocking::Client;
 use reqwest::StatusCode;
-use serde::{Deserialize, Serialize};
 
-use crate::domain::telegram::gateway::dto;
-use crate::domain::telegram::gateway::contract;
-use crate::domain::telegram::gateway::dto::response::{FailedSendMessageDto, SuccessSendMessageDto};
-use crate::domain::telegram::gateway::r#enum::endpoint::Endpoint;
+use crate::telegram::domain::repository;
+use crate::telegram::domain::model;
+use crate::telegram::domain::contract;
+use crate::telegram::domain::r#enum::endpoint::Endpoint;
 
-pub struct HttpRepository {
+pub struct Telegram {
     // telegram bot url
     endpoint: String,
     // specific telegram bot token
@@ -20,20 +17,20 @@ pub struct HttpRepository {
     methods: HashMap<Endpoint, String>
 }
 
-impl HttpRepository {
+impl Telegram {
     pub fn new(endpoint: String, token: String) -> Self {
         let mut methods: HashMap<Endpoint, String> = HashMap::new();
             methods.insert(Endpoint::GetMessagesMethod, "getUpdates".to_string());
             methods.insert(Endpoint::SendMessageMethod, "sendMessage".to_string());
 
-        return HttpRepository { endpoint, token, methods};
+        return Telegram { endpoint, token, methods};
     }
 }
 
-impl contract::gateway::GatewayInterface for HttpRepository {
-    fn get_messages<T>(&self, request: T) -> Result<dto::response::GetMessagesDto, String>
-    where
-        T: contract::request::GetMessagesDtoInterface
+impl repository::interface::Telegram for Telegram
+{
+    fn get_messages(&self, request: Box<dyn contract::request::GetMessagesDtoInterface>)
+        -> Result<model::response::GetMessagesDto, String>
     {
         let method = match self.methods.get(&Endpoint::GetMessagesMethod) {
             Some(val) => val,
@@ -49,7 +46,7 @@ impl contract::gateway::GatewayInterface for HttpRepository {
             Ok(response) => {
                 let bytes = response.bytes().unwrap();
                 let json = std::str::from_utf8(&bytes).unwrap();
-                let obj: dto::response::GetMessagesDto = serde_json::from_str(json).unwrap();
+                let obj: model::response::GetMessagesDto = serde_json::from_str(json).unwrap();
 
                 return Ok(obj);
             },
@@ -57,19 +54,24 @@ impl contract::gateway::GatewayInterface for HttpRepository {
         }
     }
 
-    fn send_message<T>(&self, request: T) -> Result<SuccessSendMessageDto, String>
-    where
-        T: contract::request::SendMessageDtoInterface + Serialize + Debug
+    fn send_message(&self, request: Box<dyn contract::request::SendMessageDtoInterface>)
+        -> Result<model::response::SuccessSendMessageDto, String>
     {
         let method = match self.methods.get(&Endpoint::SendMessageMethod) {
             Some(val) => val,
             None => panic!("fatal: endpoint for send message not found into the map")
         };
 
+        let chat_id = &request.get_chat_id().to_string();
+        let mut body: HashMap<&str, &str> = HashMap::new();
+        body.insert("text", request.get_message());
+        body.insert("chat_id", chat_id);
+        body.insert("parse_mod", request.get_parse_mod());
+
         let result = Client::new()
             .post(format!("{}/bot{}/{}", self.endpoint, self.token, method))
             .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .body(serde_json::to_string(&request).unwrap())
+            .body(serde_json::to_string(&body).unwrap())
             .send();
 
         return match result {
@@ -78,13 +80,13 @@ impl contract::gateway::GatewayInterface for HttpRepository {
                 let bytes = response.bytes().unwrap();
                 let json = std::str::from_utf8(&bytes).unwrap();
 
-                return match status_code {
+                match status_code {
                     StatusCode::OK => {
-                        let success_resp: SuccessSendMessageDto = serde_json::from_str(json).unwrap();
+                        let success_resp: model::response::SuccessSendMessageDto = serde_json::from_str(json).unwrap();
                         return Ok(success_resp);
                     },
                     _ => {
-                        let failed_resp: FailedSendMessageDto = serde_json::from_str(json).unwrap();
+                        let failed_resp: model::response::FailedSendMessageDto = serde_json::from_str(json).unwrap();
                         return Err(format!("status_code: {}, description: {}", failed_resp.error_code, failed_resp.description));
                     }
                 };
