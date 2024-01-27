9705b9400a97caff2cf207a8a2cdcd71cfd1a8db
Boris <Glazunov2142@icloud.com>
2024-01-27 12:17:55 +0300

    chore: improved facade interface.

--- src/telegram/app/facade_interface.rs
+++ src/telegram/app/facade_interface.rs
@@ -1,5 +1,7 @@
+use std::fmt::Debug;
+use serde::Serialize;
+use crate::telegram::domain::{contract, model};
 use super::service::sender::sender_interface::MessageSender;
 use super::service::receiver::receiver_interface::MessageReceiver;
 pub trait TelegramFacadeInterface: MessageSender + MessageReceiver {
-
 }
