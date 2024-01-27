2a4021860f75a7fd5dafa3aa9a26d4362e389cd9
Boris <Glazunov2142@icloud.com>
2024-01-27 12:19:23 +0300

    chore: changed repository interface.

--- src/telegram/domain/repository/interface.rs
+++ src/telegram/domain/repository/interface.rs
@@ -1,11 +1,12 @@
 use crate::telegram::domain::{contract, model};
+use crate::telegram::domain::r#enum::error::Error;
 
 pub trait Telegram {
     // receiving messages from telegram channel
     fn get_messages(&self, request: Box<dyn contract::request::GetMessagesDtoInterface>)
-        -> Result<model::response::GetMessagesDto, String>;
+        -> Result<model::response::GetMessagesDto, Error>;
 
     // sending message to telegram channel
     fn send_message(&self, request: Box<dyn contract::request::SendMessageDtoInterface>)
-        -> Result<model::response::SuccessSendMessageDto, String>;
+        -> Result<model::response::SuccessSendMessageDto, Error>;
 }
