54f17e6d97bfbb5265d7cd87a4d9c293aa7c850b
Boris <Glazunov2142@icloud.com>
2024-01-29 15:45:00 +0300

    chore: telegram facade resolved.

--- src/telegram/app/facade.rs
+++ src/telegram/app/facade.rs
@@ -1,7 +1,10 @@
+use std::future::Future;
+use std::pin::Pin;
 use std::rc::Rc;
 use crate::telegram::app::facade_interface::TelegramFacadeInterface;
 use crate::telegram::app::service::receiver::receiver_interface::MessageReceiver;
 use crate::telegram::app::service::sender::sender_interface::MessageSender;
+use crate::telegram::domain::{contract, model};
 use crate::telegram::domain::contract::request::{GetMessagesDtoInterface, SendMessageDtoInterface};
 use crate::telegram::domain::model::response::{GetMessagesDto, SuccessSendMessageDto};
 use crate::telegram::domain::r#enum::error::Error;
@@ -21,13 +24,17 @@ impl TelegramFacadeInterface for TelegramFacade {
 }
 
 impl MessageSender for TelegramFacade {
-    fn send_message(&self, request: Box<dyn SendMessageDtoInterface>) -> Result<SuccessSendMessageDto, Error> {
+    fn send_message(&self, request: Box<dyn SendMessageDtoInterface>)
+        -> Pin<Box<dyn Future<Output = Result<SuccessSendMessageDto, Error>> + '_>>
+    {
         self.sender.send_message(request)
     }
 }
 
 impl MessageReceiver for TelegramFacade {
-    fn get_messages(&self, request: Box<dyn GetMessagesDtoInterface>) -> Result<GetMessagesDto, Error> {
+    fn get_messages(&self, request: Box<dyn GetMessagesDtoInterface>)
+        -> Pin<Box<dyn Future<Output = Result<GetMessagesDto, Error>> + '_>>
+    {
         self.receiver.get_messages(request)
     }
 }
