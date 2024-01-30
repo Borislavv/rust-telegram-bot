a96a2c2119f581ee09211e2ec3c26bdd9e449cb4
Boris <Glazunov2142@icloud.com>
2024-01-30 15:45:00 +0300

    chore: telegram sender interface service resolved.

--- src/telegram/app/service/sender/sender_interface.rs
+++ src/telegram/app/service/sender/sender_interface.rs
@@ -1,11 +1,10 @@
-
-
-
+use std::future::Future;
+use std::pin::Pin;
 use crate::telegram::domain::model;
 use crate::telegram::domain::contract;
 use crate::telegram::domain::r#enum::error::Error;
 
 pub trait MessageSender {
     fn send_message(&self, request: Box<dyn contract::request::SendMessageDtoInterface>)
-       -> Result<model::response::SuccessSendMessageDto, Error>;
+        -> Pin<Box<dyn Future<Output = Result<model::response::SuccessSendMessageDto, Error>> + '_>>;
 }
