b5a465b8d6e3aeffc23b182e2d657ebd7e217dc4
Boris <Glazunov2142@icloud.com>
2024-01-31 08:48:32 +0300

    chore: telegram repository interface resolved.

--- src/telegram/domain/repository/interface.rs
+++ src/telegram/domain/repository/interface.rs
@@ -1,12 +1,14 @@
+use std::future::Future;
+use std::pin::Pin;
 use crate::telegram::domain::{contract, model};
 use crate::telegram::domain::r#enum::error::Error;
 
 pub trait Telegram {
     // receiving messages from telegram channel
     fn get_messages(&self, request: Box<dyn contract::request::GetMessagesDtoInterface>)
-        -> Result<model::response::GetMessagesDto, Error>;
+        -> Pin<Box<dyn Future<Output = Result<model::response::GetMessagesDto, Error>> + '_>>;
 
     // sending message to telegram channel
     fn send_message(&self, request: Box<dyn contract::request::SendMessageDtoInterface>)
-        -> Result<model::response::SuccessSendMessageDto, Error>;
+        -> Pin<Box<dyn Future<Output = Result<model::response::SuccessSendMessageDto, Error>> + '_>>;
 }
