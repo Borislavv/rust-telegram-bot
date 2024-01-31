40a95635d117da57953e44b7694551f9570ce8cf
Boris <Glazunov2142@icloud.com>
2024-01-31 08:48:53 +0300

    chore: telegram http repository resolved.

--- src/telegram/infrastructure/repository/http.rs
+++ src/telegram/infrastructure/repository/http.rs
@@ -1,7 +1,9 @@
 use std::collections::HashMap;
+use std::future::Future;
+use std::pin::Pin;
+use std::sync::Arc;
 
-use reqwest::blocking::Client;
-use reqwest::StatusCode;
+use reqwest::{Client, StatusCode};
 
 use crate::telegram::domain::repository;
 use crate::telegram::domain::model;
@@ -15,74 +17,72 @@ pub struct Telegram {
     endpoint: String,
     // specific telegram bot token
     token: String,
-    // telegram api methods
-    methods: HashMap<Endpoint, String>
 }
 
 impl Telegram {
     pub fn new(endpoint: String, token: String) -> Self {
-        let mut methods: HashMap<Endpoint, String> = HashMap::new();
-            methods.insert(Endpoint::GetMessagesMethod, "getUpdates".to_string());
-            methods.insert(Endpoint::SendMessageMethod, "sendMessage".to_string());
-
-        return Telegram { endpoint, token, methods};
+        return Telegram { endpoint, token };
     }
 }
 
 impl repository::interface::Telegram for Telegram
 {
     fn get_messages(&self, request: Box<dyn contract::request::GetMessagesDtoInterface>)
-        -> Result<model::response::GetMessagesDto, Error>
+        -> Pin<Box<dyn Future<Output = Result<model::response::GetMessagesDto, Error>> + '_>>
     {
-        let method = match self.methods.get(&Endpoint::GetMessagesMethod) {
-            Some(val) => val,
-            None => panic!("fatal: endpoint for request messages not found into the map")
-        };
+        let url = format!("{}/bot{}/{}", self.endpoint, self.token, Endpoint::GetMessagesMethod.as_str());
 
-        let response = Client::new()
-            .get(format!("{}/bot{}/{}", self.endpoint, self.token, method))
-            .query(&[("offset", request.get_offset())])
-            .send()?;
+        Box::pin(
+            async move {
+                let response = Client::new()
+                    .get(url)
+                    .query(&[("offset", request.get_offset())])
+                    .send()
+                    .await?;
 
-        let status_code = response.status();
-        let bytes = response.bytes()?;
-        let json = std::str::from_utf8(&bytes)?;
+                let status_code = response.status();
+                let bytes = response.bytes().await?;
+                let json = std::str::from_utf8(&bytes)?;
 
-        println!("{}", json);
+                println!("{}", json);
 
-        return match status_code {
-            StatusCode::OK => Ok(serde_json::from_str::<GetMessagesDto>(json)?),
-            _ => Err(Error::TelegramError(serde_json::from_str::<FailedResponseDto>(json)?.description)),
-        };
+                match status_code {
+                    StatusCode::OK => Ok(serde_json::from_str::<GetMessagesDto>(json)?),
+                    _ => Err(Error::TelegramError(serde_json::from_str::<FailedResponseDto>(json)?.description)),
+                }
+            }
+        )
     }
 
     fn send_message(&self, request: Box<dyn contract::request::SendMessageDtoInterface>)
-        -> Result<model::response::SuccessSendMessageDto, Error>
+        -> Pin<Box<dyn Future<Output = Result<model::response::SuccessSendMessageDto, Error>> + '_>>
     {
-        let method = match self.methods.get(&Endpoint::SendMessageMethod) {
-            Some(val) => val,
-            None => panic!("fatal: endpoint for send message not found into the map")
-        };
+        let url = format!("{}/bot{}/{}", self.endpoint, self.token, Endpoint::SendMessageMethod.as_str());
 
-        let chat_id = &request.get_chat_id().to_string();
-        let mut body: HashMap<&str, &str> = HashMap::new();
-        body.insert("text", request.get_message());
-        body.insert("chat_id", chat_id);
-        body.insert("parse_mod", request.get_parse_mod());
+        Box::pin(
+            async move {
+                let chat_id = request.get_chat_id().to_string();
+                let mut body: HashMap<&str, &str> = HashMap::new();
+                body.insert("text", request.get_message());
+                body.insert("chat_id", chat_id.as_str());
+                body.insert("parse_mod", request.get_parse_mod());
 
-        let response = Client::new()
-            .post(format!("{}/bot{}/{}", self.endpoint, self.token, method))
-            .header(reqwest::header::CONTENT_TYPE, "application/json")
-            .body(serde_json::to_string(&body)?)
-            .send()?;
+                let response = Client::new()
+                    .post(url)
+                    .header(reqwest::header::CONTENT_TYPE, "application/json")
+                    .body(serde_json::to_string(&body)?)
+                    .send()
+                    .await?;
 
-        let status_code = response.status();
-        let bytes = response.bytes().unwrap();
-        let json = std::str::from_utf8(&bytes)?;
+                let status_code = response.status();
+                let bytes = response.bytes().await?;
+                let json = std::str::from_utf8(&bytes)?;
 
-        return match status_code {
-            StatusCode::OK => Ok(serde_json::from_str::<SuccessSendMessageDto>(json)?),
-            _ => Err(Error::TelegramError(serde_json::from_str::<FailedResponseDto>(json)?.description)),
-        };
+                match status_code {
+                    StatusCode::OK => Ok(serde_json::from_str::<SuccessSendMessageDto>(json)?),
+                    _ => Err(Error::TelegramError(serde_json::from_str::<FailedResponseDto>(json)?.description)),
+                }
+            }
+        )
     }
 }
